0998970957f349a5c1612e886eac7fc87522f25f
Andreas Fried <AndrolinRoot@googlemail.com>
2014-02-14 19:40:14 +0100

    Update..

--- configs/common.mk
+++ configs/common.mk
@@ -1,25 +1,23 @@
 SUPERUSER_EMBEDDED := true
 
 # Common overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/vanilla/overlay/common
 
 # Common dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/vanilla/overlay/dictionaries
 
 PRODUCT_PACKAGES += \
     BluetoothExt \
     CellBroadcastReceiver \
     libemoji \
     LatinImeDictionaryPack \
-    mGerrit \
     Microbes \
     ROMControl \
     Stk \
     su \
-    SwagPapers \
+    VanillaPapers \
     Torch \
-    UnicornPorn \
-    KitKatWhite
+    UnicornPorn 
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -44,21 +42,21 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Installer
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/bin/persist.sh:install/bin/persist.sh \
-    vendor/aokp/prebuilt/common/etc/persist.conf:system/etc/persist.conf
+    vendor/vanilla/prebuilt/common/bin/persist.sh:install/bin/persist.sh \
+    vendor/vanilla/prebuilt/common/etc/persist.conf:system/etc/persist.conf
 
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/lib/libmicrobes_jni.so:system/lib/libmicrobes_jni.so \
-    vendor/aokp/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf
+    vendor/vanilla/prebuilt/common/lib/libmicrobes_jni.so:system/lib/libmicrobes_jni.so \
+    vendor/vanilla/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf
 
 # init.d
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/etc/init.local.rc:root/init.aokp.rc \
-    vendor/aokp/prebuilt/common/etc/init.d/00start:system/etc/init.d/00start \
-    vendor/aokp/prebuilt/common/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
-    vendor/aokp/prebuilt/common/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel \
-    vendor/aokp/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf \
-    vendor/aokp/prebuilt/common/bin/sysinit:system/bin/sysinit
+    vendor/vanilla/prebuilt/common/etc/init.local.rc:root/init.aokp.rc \
+    vendor/vanilla/prebuilt/common/etc/init.d/00start:system/etc/init.d/00start \
+    vendor/vanilla/prebuilt/common/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
+    vendor/vanilla/prebuilt/common/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel \
+    vendor/vanilla/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf \
+    vendor/vanilla/prebuilt/common/bin/sysinit:system/bin/sysinit
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
@@ -91,15 +89,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Inherit common build.prop overrides
--include vendor/aokp/configs/common_versions.mk
+-include vendor/vanilla/configs/common_versions.mk
 
 # T-Mobile theme engine
--include vendor/aokp/configs/themes_common.mk
+-include vendor/vanilla/configs/themes_common.mk
 
 # common boot animation
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+    vendor/vanilla/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 
 # World APNs
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/vanilla/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
