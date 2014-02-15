f1e76a620439ef8b4b787896a61017e0f3c1d226
Andreas Fried <AndrolinRoot@googlemail.com>
2014-02-15 12:09:35 +0100

    Update common_versions.mk

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -4,28 +4,20 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-bui
 DATE = $(shell vendor/aokp/tools/getdate)
 AOKP_BRANCH=kitkat
 
-ifneq ($(AOKP_BUILD),)
-    # AOKP_BUILD=<goo version int>/<build string>
-    PRODUCT_PROPERTY_OVERRIDES += \
-        ro.goo.developerid=aokp \
-        ro.goo.rom=aokp \
-        ro.goo.version=$(shell echo $(AOKP_BUILD) | cut -d/ -f1) \
-        ro.aokp.version=$(TARGET_PRODUCT)_$(AOKP_BRANCH)_$(shell echo $(AOKP_BUILD) | cut -d/ -f2)
-else
     ifneq ($(AOKP_NIGHTLY),)
-        # AOKP_NIGHTLY=true
+        # VU_NIGHTLY=true
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.aokp.version=$(TARGET_PRODUCT)_$(AOKP_BRANCH)_nightly_$(DATE)
+            ro.vu.version=$(TARGET_PRODUCT)_$(VU_BRANCH)_nightly_$(DATE)
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.aokp.version=$(TARGET_PRODUCT)_$(AOKP_BRANCH)_unofficial_$(DATE)
+            ro.vu.version=$(TARGET_PRODUCT)_$(VU_BRANCH)_unofficial_$(DATE)
     endif
 endif
 
 # needed for statistics
 PRODUCT_PROPERTY_OVERRIDES += \
-        ro.aokp.branch=$(AOKP_BRANCH) \
-        ro.aokp.device=$(AOKP_PRODUCT)
+        ro.aokp.branch=$(VU_BRANCH) \
+        ro.aokp.device=$(VU_PRODUCT)
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
