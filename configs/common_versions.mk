32053a4285c7bedcb5373036405a645eb3c216c6
Andreas Fried <AndrolinRoot@googlemail.com>
2014-02-15 21:42:53 +0100

    Fixes

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,22 +1,13 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
-DATE = $(shell vendor/aokp/tools/getdate)
-AOKP_BRANCH=kitkat
-
-    ifneq ($(AOKP_NIGHTLY),)
-        # VU_NIGHTLY=true
-        PRODUCT_PROPERTY_OVERRIDES += \
-            ro.vu.version=$(TARGET_PRODUCT)_$(VU_BRANCH)_nightly_$(DATE)
-    else
-        PRODUCT_PROPERTY_OVERRIDES += \
-            ro.vu.version=$(TARGET_PRODUCT)_$(VU_BRANCH)_unofficial_$(DATE)
-endif
+DATE = $(shell vendor/vanilla/tools/getdate)
+VU_BRANCH=kitkat
 
 # needed for statistics
 PRODUCT_PROPERTY_OVERRIDES += \
-        ro.aokp.branch=$(VU_BRANCH) \
-        ro.aokp.device=$(VU_PRODUCT)
+        ro.vu.branch=$(VU_BRANCH) \
+        ro.vu.device=$(VU_PRODUCT)
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
