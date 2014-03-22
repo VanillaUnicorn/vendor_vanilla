8c87c7ac73c5ea22131ddbaadb34da0c0a54f4d9
Andreas Fried <AndrolinRoot@googlemail.com>
2014-03-22 21:19:32 +0100

    add_lunch_combo vu_dlx-eng

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,6 +10,7 @@ add_lunch_combo vu_mako-userdebug
 add_lunch_combo vu_maguro-userdebug
 add_lunch_combo vu_flo-userdebug
 add_lunch_combo vu_m7-userdebug
+add_lunch_combo vu_dlx-eng
 add_lunch_combo aokp_toro-userdebug
 add_lunch_combo aokp_manta-userdebug
 add_lunch_combo aokp_grouper-userdebug
