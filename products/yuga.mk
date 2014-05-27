2d77d5be9b3b700d184ecbe90ed1196e2cd95318
Andreas Fried <AndrolinRoot@googlemail.com>
2014-05-27 21:07:22 +0200

    Bootanimation

--- products/yuga.mk
+++ products/yuga.mk
@@ -13,4 +13,4 @@ PRODUCT_MANUFACTURER := Sony
 
 # boot animation
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
+    vendor/vanilla/prebuilt/bootanimation/bootanimation_720_720.zip:system/media/bootanimation-alt.zip
