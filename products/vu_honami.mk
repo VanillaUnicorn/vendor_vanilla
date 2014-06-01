86577f243935c8b0f58a3b0831ef6cb1e3d995f1
Andreas Fried <AndrolinRoot@googlemail.com>
2014-06-01 15:10:23 +0200

    Add VU_Honami Support

--- products/vu_honami.mk
+++ products/vu_honami.mk
@@ -1,11 +1,11 @@
 # Inherit AOSP device configuration for honami
-$(call inherit-product-if-exists, device/sony/honami/aokp.mk)
+$(call inherit-product-if-exists, device/sony/honami/vu.mk)
 
 # honami overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/honami
+PRODUCT_PACKAGE_OVERLAYS += vendor/vanilla/overlay/honami
 
 # Setup device specific product configuration.
-PRODUCT_NAME := aokp_honami
+PRODUCT_NAME := vu_honami
 PRODUCT_BRAND := Sony
 PRODUCT_DEVICE := honami
 PRODUCT_MODEL := Xperia Z1
@@ -13,4 +13,4 @@ PRODUCT_MANUFACTURER := Sony
 
 # boot animation
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
+    vendor/vanilla/prebuilt/bootanimation/bootanimation_720_720.zip:system/media/bootanimation-alt.zip
